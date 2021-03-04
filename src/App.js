1a5899bd6c031c70490dfe19433af9bff47c9322
theo <tperdriel@gmail.com>
2021-03-04 13:44:08 +0100

    new tuto for keycloak

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
-import React from "react";
-import "./style.css";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Dashboard from './Dashboard';
-import Login from './Login'
-import Preference from './Preference';
-import useToken from './useToken'
-
-
-
-
 function App() {
-  const { token, setToken } = useToken();
-
-  if (!token) {
-    return <Login setToken={setToken}/>
-  }
-
   return (
-    <div className="wrapper">
+    <div>
       <h1> Application</h1>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard/>
-          </Route>
-          <Route path="/preference">
-            <Preference/>
-          </Route>
-        </Switch>
-      </BrowserRouter>
     </div>
   );
 }

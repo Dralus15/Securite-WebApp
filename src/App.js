b258804ea5dbe098427347979aa33689982cafb8
wangwei <neuwangwei96@gmail.com>
2021-03-04 22:54:57 +0100

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
-import React from "react";
-import "./style.css";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Dashboard from './Dashboard';
-import Login from './Login'
-import Preference from './Preference';
-import useToken from './useToken'
-
-
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const { token, setToken } = useToken();
-
-  if (!token) {
-    return <Login setToken={setToken}/>
-  }
-
   return (
-    <div className="wrapper">
-      <h1> Application</h1>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard/>
-          </Route>
-          <Route path="/preference">
-            <Preference/>
-          </Route>
-        </Switch>
-      </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }

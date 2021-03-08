3544f75cb92c42cb52ddff38c9c4020283f1ace0
wangwei <neuwangwei96@gmail.com>
2021-03-08 15:16:45 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/**import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -22,4 +22,29 @@ function App() {
   );
 }
 
+export default App;*/
+
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Welcome from './Welcome';
+import Secured from './Secured';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+        <BrowserRouter>
+          <div className="container">
+            <ul>
+              <li><Link to="/">public component</Link></li>
+              <li><Link to="/secured">secured component</Link></li>
+            </ul>
+            <Route exact path="/" component={Welcome} />
+            <Route path="/secured" component={Secured} />
+          </div>
+        </BrowserRouter>
+    );
+  }
+}
 export default App;

af4d3b084fe1c4121b89f109abbd16104415f0fb
theo <tperdriel@gmail.com>
2021-03-22 14:20:18 +0100

    form front in welcome component, server doesn't work yet

--- src/Secured.js
+++ src/Secured.js
@@ -1,34 +1,36 @@
-import React, { Component } from 'react';
-import Keycloak from 'keycloak-js';
+import React, { Component } from "react";
+import Keycloak from "keycloak-js";
 
 class Secured extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { keycloak: null, authenticated: false };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = { keycloak: null, authenticated: false };
-    }
+  componentDidMount() {
+    console.log("componentDidMount 1");
+    const keycloak = Keycloak("keycloak.json");
+    keycloak.init({ onLoad: "login-required" }).then((authenticated) => {
+      console.log("componentDidMount 2");
+      this.setState({ keycloak: keycloak, authenticated: authenticated });
+    });
+  }
 
-    componentDidMount() {
-        console.log("componentDidMount 1");
-        const keycloak = Keycloak('keycloak.json');
-        keycloak.init({onLoad: 'login-required'}).then(authenticated => {
-            console.log("componentDidMount 2");
-            this.setState({ keycloak: keycloak, authenticated: authenticated })
-        })
-    }
-
-    render() {
-        if (this.state.keycloak) {
-            if (this.state.authenticated) return (
-                <div>
-                    <p>This is a Keycloak-secured component of your application. You shouldn't be able
-                        to see this unless you've authenticated with Keycloak.</p>
-                </div>
-            ); else return (<div>Unable to authenticate!</div>)
-        }
+  render() {
+    if (this.state.keycloak) {
+      if (this.state.authenticated)
         return (
-            <div>Initializing Keycloak...</div>
+          <div>
+            <p>
+              This is a Keycloak-secured component of your application. You
+              shouldn't be able to see this unless you've authenticated with
+              Keycloak.
+            </p>
+          </div>
         );
+      else return <div>Unable to authenticate!</div>;
     }
+    return <div>Initializing Keycloak...</div>;
+  }
 }
 export default Secured;
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,12 +1,102 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 
-class Welcome extends Component {
-    render() {
+export default function Welcome() {
+  let [dataList, setDataList] = useState([]);
+  let [nbObject, setNbObject] = useState(0);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    let dataObject = {
+      idData: nbObject,
+      nomCarte: event.target.nomCarte.value,
+      numeroCarte: event.target.numeroCarte.value,
+      dateCarte: event.target.dateCarte.value,
+      codeSecuCarte: event.target.codeSecuCarte.value,
+    };
+
+    dataList.push(dataObject);
+    setDataList(dataList);
+    setNbObject(nbObject + 1);
+
+    // var form = new FormData(document.getElementById("dataForm"));
+
+    // fetch("http://localhost:8080/", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     dataObject: dataObject,
+    //   }),
+    // }).then(
+    //   function (res) {
+    //     if (res.ok) {
+    //       alert("Perfect! ");
+    //     } else if (res.status === 401) {
+    //       alert("Oops! ");
+    //     }
+    //   },
+    //   function (e) {
+    //     alert("Error submitting form!");
+    //   }
+    // );
+  };
+
+  return (
+    <div>
+      <p>public component</p>
+      <form onSubmit={handleSubmit}>
+        <p>Entrez le nom associé à la carte</p>
+        <input
+          autoComplete="off"
+          type="text"
+          name="nomCarte"
+          id="nomCarte"
+          required
+        ></input>
+        <br />
+        <p>Entrez le numéro associé à la carte</p>
+        <input
+          autoComplete="off"
+          type="text"
+          name="numeroCarte"
+          id="numeroCarte"
+          required
+        ></input>
+        <br />
+        <p>Entrez la date de validité</p>
+        <input
+          autoComplete="off"
+          type="text"
+          name="dateCarte"
+          id="dateCarte"
+          required
+        ></input>
+        <br />
+        <p>Entrez le code de sécurité</p>
+        <input
+          autoComplete="off"
+          type="text"
+          name="codeSecuCarte"
+          id="codeSecuCarte"
+          required
+        ></input>
+        <br />
+        <input type="submit" value="Subscribe!"></input>
+      </form>
+      <p>il y a : {nbObject} informations entrées</p>
+      {dataList.map((dataObject) => {
         return (
-            <div className="Welcome">
-                <p>This is your public-facing component.</p>
-            </div>
+          <div key={dataObject.idData}>
+            <p>information bancaire n°{dataObject.idData}</p>
+            <p>nom : {dataObject.nomCarte}</p>
+            <p>numero : {dataObject.numeroCarte}</p>
+            <p>date validité : {dataObject.dateCarte}</p>
+            <p>code sécurité : {dataObject.codeSecuCarte}</p>
+            <p>........................</p>
+          </div>
         );
-    }
+      })}
+    </div>
+  );
 }
-export default Welcome;

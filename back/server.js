0656784c3fc82e2ff873e671affd1e1f318048f8
theo <tperdriel@gmail.com>
2021-03-26 17:30:15 +0100

    correction bug and random id

--- back/server.js
+++ back/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var uniqid = require("uniqid");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
@@ -9,10 +10,11 @@ app.post("/api/updateData", (req, res) => {
   let fs = require("fs");
   let rawdata = fs.readFileSync("data.json");
   let data = JSON.parse(rawdata);
+  req.body.dataObject.idData = uniqid();
   data.listeInfoBancaire.push(req.body.dataObject);
   fs.writeFile(
     "data.json",
-    JSON.stringify(data.listeInfoBancaire),
+    JSON.stringify({ listeInfoBancaire: data.listeInfoBancaire }),
     "utf8",
     (err) => {
       if (err) throw err;
@@ -24,9 +26,8 @@ app.post("/api/updateData", (req, res) => {
 app.get("/api/getData", (req, res) => {
   let fs = require("fs");
   let rawdata = fs.readFileSync("data.json");
-  let listeInfoBancaire = JSON.parse(rawdata);
-  console.log(listeInfoBancaire);
-  res.send({ listeInfoBancaire: listeInfoBancaire });
+  let data = JSON.parse(rawdata);
+  res.send({ listeInfoBancaire: data.listeInfoBancaire });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

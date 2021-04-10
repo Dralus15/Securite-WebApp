714f4dd8ed072fd1902464f4691f7a03a0079280
Dralus15 <v.lefillastre@gmail.com>
2021-04-10 15:42:38 +0200

    Revert "KeycloakMain"
    This reverts commit 67892f21

--- src/Dashboard.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./style.css";
-
-export default function Dashboard() {
-  return (<h2>Dashboard</h2>);
-}
--- src/Preference.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./style.css";
-
-export default function Preference() {
-  return (<h2>Preference</h2>);
-}
